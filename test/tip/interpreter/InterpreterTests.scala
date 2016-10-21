af6811e3dc4798ee1b394e992c480db6ab888306
Coen De Roover <cderoove@vub.ac.be>
2016-10-21 09:13:44 -0400

    Added a simple JUnit tests for the TIP interpreter.

---
+++ test/tip/interpreter/InterpreterTests.scala
@@ -0,0 +1,43 @@
+package tip.interpreter
+
+import tip.ast.AProgram
+import tip.parser.TipParser
+import tip.analysis.DeclarationAnalysis
+import scala.util.Success
+import scala.io.Source
+import org.junit.Test
+import org.junit.Assert._
+import java.io.ByteArrayInputStream
+  
+class InterpreterTests {
+
+	def prepare(fileName : String) : AProgram =  {
+			val src = Source.fromFile(fileName).mkString
+		  val tipParser = new TipParser(src)
+			val p = tipParser.InputLine.run()
+			p match {
+			  case Success(programNode : tip.ast.AProgram) => {
+				  //required for side effects on environment
+				  new DeclarationAnalysis(programNode)
+				  programNode
+			  }
+			}
+	}
+	
+	@Test
+	def factorialRecursiveIterative() {
+	  val in = new ByteArrayInputStream(("5\n4").getBytes)
+		val recursive = ("tipprograms/factorial_iterative.tip")
+		val iterative = ("tipprograms/factorial_recursive.tip")
+		val progrec = prepare(recursive)
+		val progite = prepare(iterative)
+		Console.withIn(in)  {
+		  val resrec = new Interpreter(progrec).run()   
+		  assertEquals(resrec, 120)
+		  val resite = new Interpreter(progite).run()
+		  assertEquals(resite, 24)
+	  }
+	}
+	
+	
+}
